b8a4127e76e9957cf332d80cc39647bb33e9f05e
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-10-15 09:59:24 -0400

    Added "multiply" to main method

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -7,13 +7,22 @@ public class Main {
     public static void main(String[] args) {
         // write your code here
         String aName = theName("Whaler");
-
         System.out.println("Hello World");
         System.out.println(aName);
+        int a,b;
+        a=10;
+        b=15;
+        int answer = multiply(a,b);
+        System.out.println(answer);
+
     }
 
         public static String theName(String name){
         return "Hello" + name ;
+    }
+
+    public static int multiply (int a, int b){
+        return a*b;
     }
 
     
