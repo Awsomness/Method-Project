06c0da60bd5ac6792e19ad0a86bcb01e8162f92c
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-10-15 09:43:29 -0400

    Added "Hello Whaler" to main method

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -1,11 +1,19 @@
 package org.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+        String aName = theName("Whaler");
 
         System.out.println("Hello World");
+        System.out.println(aName);
+    }
+
+        public static String theName(String name){
+        return "Hello" + name ;
     }
 
     
