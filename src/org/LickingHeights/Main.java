6fe4f36132b478a3eb80a4cb3ed2fa7324749f51
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-10-15 09:22:18 -0400

    Added "Hello World" to main method

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -2,14 +2,13 @@ package org.LickingHeights;
 
 public class Main {
 
+    public static void main(String[] args) {
+	// write your code here
+
+        System.out.println("Hello World");
+    }
 
     
 
-
-
-
-
-    public static void main(String[] args) {
-	// write your code here
-    }
 }
+
