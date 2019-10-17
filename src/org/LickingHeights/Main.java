074e76bb7a8b889233980e517b4d0bc9b275c02e
Nathanial <nathanial.kilpatrick.2022@student.lhschools.org>
2019-10-17 09:19:46 -0400

    Added "Divide" to main method

--- src/org/LickingHeights/Main.java
+++ src/org/LickingHeights/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        String aName = theName("Whaler");
+        String aName = theName("Whalter");
         System.out.println("Hello World");
         System.out.println(aName);
         int a,b;
@@ -14,15 +14,24 @@ public class Main {
         b=15;
         int answer = multiply(a,b);
         System.out.println(answer);
+        int c,d;
+        c=1;
+        d=5;
+        double answer2 = divide(c,d);
+        System.out.println(answer2);
 
     }
 
         public static String theName(String name){
-        return "Hello" + name ;
+        return "Hello " + name ;
     }
 
     public static int multiply (int a, int b){
         return a*b;
+    }
+
+    public static double divide (double c, double d){
+        return c/d;
     }
 
     
